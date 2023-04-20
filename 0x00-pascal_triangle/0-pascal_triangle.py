e1fb31be50e8090890f809b2954eec2502a6300e
bill omole <omolebill01@gmail.com>
2023-04-20 11:14:09 +0200

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""This scripts prints the pascal triangle"""
+
+
+def pascal_triangle(n):
+    """We retrun the pascal array"""
+    if n == 0:
+        return ([])
+    tringle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = tringle[i - 1][j - 1] +  tringle[i - 1][j]
+        tringle.append(row)
+    return tringle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,2 @@
+## Pascal Triangle
+The pascal triangle in python is a triangle number pattern where the outer periphery of the triangle is always one and the inner values of the traingles are obtained by the adding the adjacent rows
