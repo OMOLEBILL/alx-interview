03a293cb4650f29b30488214e3afae8197f5435a
bill omole <omolebill01@gmail.com>
2023-05-03 11:30:16 +0200

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This module tries to find the minimum operations used 
+    to achieve a copy of H
+"""
+
+
+def minOperations(n: int) -> int:
+    """ we return the minimum number of operations """
+    if n < 0:
+        return 0
+    moves = 0
+    fact = 2
+    while (n > 1):
+        if (n % fact) == 0:
+            n //= fact
+            moves += fact
+        else :
+            fact += 1
+    return moves
