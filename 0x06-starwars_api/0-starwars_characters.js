7e5670468c9418e3b7f471dd2ac8ba4a1dd21a39
bill omole <omolebill01@gmail.com>
2023-06-02 00:25:45 +0200

    star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,10 +3,10 @@ const request = require('request');
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2],
   async function (err, res, body) {
     if (err) return console.error(err);
-    const urlList = JSON.parse(body).characters;
-    for (const url of urlList) {
+    const charURLList = JSON.parse(body).characters;
+    for (const charURL of charURLList) {
       await new Promise(function (resolve, reject) {
-        request(url, function (err, res, body) {
+        request(charURL, function (err, res, body) {
           if (err) return console.error(err);
           console.log(JSON.parse(body).name);
           resolve();
