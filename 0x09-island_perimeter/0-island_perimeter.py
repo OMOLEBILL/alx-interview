f8c15b2c099ce857d38c11a4b90c024044778262
bill omole <omolebill01@gmail.com>
2023-06-22 22:13:23 +0200

    island perimeter rework

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,36 +7,14 @@ def island_perimeter(grid):
     perimeter = 0
     for index, value in enumerate(grid):
         if 1 in value:
-            for j in range(len(value) - 1):
+            for j in range(len(value)):
                 if value[j] == 1:
-                    if index == 0:
-                        if j > 0:
-                            if value[j - 1] == 0:
-                                perimeter += 1
-                            if value[j + 1] == 0:
-                                perimeter += 1
-                            if grid[index + 1][j] == 0:
-                                perimeter += 1
-                        else:
-                            if value[j + 1] == 0:
-                                perimeter += 1
-                            if grid[index + 1][j] == 0:
-                                perimeter += 1
-                    else:
-                        if j > 0:
-                            if value[j - 1] == 0:
-                                perimeter += 1
-                            if value[j + 1] == 0:
-                                perimeter += 1
-                            if grid[index - 1][j] == 0:
-                                perimeter += 1
-                            if grid[index + 1][j] == 0:
-                                perimeter += 1
-                        else:
-                            if value[j + 1] == 0:
-                                perimeter += 1
-                            if grid[index - 1][j] == 0:
-                                perimeter += 1
-                            if grid[index + 1][j] == 0:
-                                perimeter += 1
+                    if value[j - 1] == 0:
+                        perimeter += 1
+                    if value[j + 1] == 0:
+                        perimeter += 1
+                    if grid[index - 1][j] == 0:
+                        perimeter += 1
+                    if grid[index + 1][j] == 0:
+                        perimeter += 1
     return perimeter
