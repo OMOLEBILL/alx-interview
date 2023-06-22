b1255bbdb3c1c6f92b287965ce932ba90b281923
bill omole <omolebill01@gmail.com>
2023-06-22 22:44:27 +0200

    format

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,16 +5,16 @@
 def island_perimeter(grid):
     """We get the perimeter of the grid"""
     perimeter = 0
-    for index, value in enumerate(grid):
-        if 1 in value:
-            for j in range(len(value)):
-                if value[j] == 1:
-                    if value[j - 1] == 0:
-                        perimeter += 1
-                    if value[j + 1] == 0:
-                        perimeter += 1
-                    if grid[index - 1][j] == 0:
-                        perimeter += 1
-                    if grid[index + 1][j] == 0:
-                        perimeter += 1
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Start with assuming all sides are land
+
+                # Check adjacent cells
+                if i > 0 and grid[i - 1][j] == 1:  # Top
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:  # Left
+                    perimeter -= 2
     return perimeter
